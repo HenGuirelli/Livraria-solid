ab34a630dadac5f7aa691be3f3506d4d55ff7c75
Henrique Guirelli <henrique.guirelli@intelitrader.com.br>
2019-05-07 04:44:23 -0300

    Atualiza nome do endpoint

--- servidor/mavenproject1/src/main/webapp/resources/scripts/venda.js
+++ servidor/mavenproject1/src/main/webapp/resources/scripts/venda.js
@@ -1,3 +1,3 @@
 const venderLivro = async (cliente, quantidade, livro, formaPagamento, dadosPagamento) => {
-     return await POST(baseURLRest + '/venda/vender/livro', { cliente, quantidade, livro, dadosPagamento, formaPagamento })
+     return await POST(baseURLRest + '/venda/vender/produto', { cliente, quantidade, livro, dadosPagamento, formaPagamento })
  }
